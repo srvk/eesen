c473aa4e058563d0870f986663ca20b462faa9da
Florian Metze <fmetze@cs.cmu.edu>
2016-02-23 23:01:03 -0500

    changes for TEDLIUM v2

--- asr_egs/tedlium/v1/path.sh
+++ asr_egs/tedlium/v1/path.sh
@@ -2,3 +2,14 @@ export EESEN_ROOT=`pwd`/../../..
 export PATH=$PWD/utils/:$EESEN_ROOT/src/netbin:$EESEN_ROOT/src/featbin:$EESEN_ROOT/src/decoderbin:$EESEN_ROOT/src/fstbin:$EESEN_ROOT/tools/openfst/bin:$EESEN_ROOT/tools/irstlm/bin/:$PWD:$PATH
 export LC_ALL=C
 
+if [[ `uname -n` =~ compute- ]]; then
+    # CMu Rocks cluster
+    module load python27
+    module load gcc-4.9.2
+fi
+
+if [[ ! -z ${acwt+x} ]]; then
+    # let's assume we're decoding
+    export PATH=$EESEN_ROOT/src-nogpu/netbin:$PATH
+    echo "Preferring non-gpu netbin code"
+fi
--- asr_egs/tedlium/v1/run_ctc_phn.sh
+++ asr_egs/tedlium/v1/run_ctc_phn.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-. ./cmd.sh ## You'll want to change cmd.sh to something that will work on your system.
-           ## This relates to the queue.
+### for CMU rocks cluster ###
+#PBS -q standard
+#PBS -j oe
+#PBS -o log
+#PBS -d .
+#PBS -V
+#PBS -l walltime=48:00:00,nodes=1:ppn=12
+
+. cmd.sh ## You'll want to change cmd.sh to something that will work on your system.
+         ## This relates to the queue.
 . path.sh
 
 stage=0
+
 . parse_options.sh
 
 if [ $stage -le 1 ]; then

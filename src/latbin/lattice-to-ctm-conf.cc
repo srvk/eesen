bdaaae8542825da2edc7a5539ccfafd566173609
Eric Riebling <er1k@cs.cmu.edu>
2016-04-18 18:48:27 +0000

    Support channel IDs other than 1

--- src/latbin/lattice-to-ctm-conf.cc
+++ src/latbin/lattice-to-ctm-conf.cc
@@ -99,6 +99,15 @@ int main(int argc, char *argv[]) {
       CompactLattice clat = clat_reader.Value();
       clat_reader.FreeCurrent();
       fst::ScaleLattice(fst::LatticeScale(lm_scale, acoustic_scale * ascale_factor), &clat);
+      std::size_t firstDash = key.find ("-");
+
+      int first = static_cast<int>(firstDash);
+
+      std::size_t nextDash = key.find ("-", first+1);
+      int next = static_cast<int>(nextDash);
+      int len = next - first;
+
+      std::string channel = key.substr(firstDash+1, len-1);
 
       MinimumBayesRisk mbr(clat, decode_mbr);
       
@@ -109,7 +118,8 @@ int main(int argc, char *argv[]) {
       KALDI_ASSERT(conf.size() == words.size() && words.size() == times.size());
       for (size_t i = 0; i < words.size(); i++) {
         KALDI_ASSERT(words[i] != 0); // Should not have epsilons.
-        ko.Stream() << key << " 1 " << (frame_shift * times[i].first) << ' '
+	
+        ko.Stream() << key << ' ' << channel << ' ' << (frame_shift * times[i].first) << ' '
                     << (frame_shift * (times[i].second-times[i].first)) << ' '
                     << words[i] << ' ' << conf[i] << '\n';
       }

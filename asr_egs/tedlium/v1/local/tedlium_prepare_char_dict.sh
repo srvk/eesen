69a983084b21ee75162f88a544600fd66276c08a
yajiemiao <yajiemiao@gmail.com>
2016-04-30 18:03:45 -0400

    bug fix in tedlium char recipe

--- asr_egs/tedlium/v1/local/tedlium_prepare_char_dict.sh
+++ asr_egs/tedlium/v1/local/tedlium_prepare_char_dict.sh
@@ -12,19 +12,17 @@ mkdir -p $dir
 [ -f path.sh ] && . ./path.sh
 
 # Use the word list of the phoneme-based lexicon. Create the lexicon using characters.
-cat $phndir/lexicon2_raw_nosil.txt | awk '{print $1}' | \
+cat $phndir/lexicon_word.txt | awk '{print $1}' | \
   perl -e 'while(<>){ chop; $str="$_"; foreach $p (split("", $_)) {$str="$str $p"}; print "$str\n";}' \
-  > $dir/lexicon2_raw_nosil.txt
+  > $dir/lexicon_word.txt
 
 #  Get the set of lexicon units without noises
-cut -d' ' -f2- $dir/lexicon2_raw_nosil.txt | tr ' ' '\n' | sort -u > $dir/units_nosil.txt
+cut -d' ' -f2- $dir/lexicon_word.txt | tr ' ' '\n' | sort -u > $dir/units_nosil.txt
 
 # Add special noises words & characters into the lexicon.
 ( echo '[BREATH] [BREATH]'; echo '[NOISE] [NOISE]'; echo '[COUGH] [COUGH]';
   echo '[SMACK] [SMACK]'; echo '[UM] [UM]'; echo '[UH] [UH]'; echo '<UNK> <UNK>'; echo '<SPACE> <SPACE>';) | \
-  cat - $dir/lexicon2_raw_nosil.txt | sort | uniq > $dir/lexicon.txt || exit 1;
-
-cat $dir/lexicon2.txt | sort -u > $dir/lexicon.txt || exit 1;
+  cat - $dir/lexicon_word.txt | sort | uniq > $dir/lexicon.txt || exit 1;
 
 # The complete set of lexicon units, indexed by numbers starting from 1
 (echo '[BREATH]'; echo '[NOISE]'; echo '[COUGH]'; echo '[SMACK]'; 

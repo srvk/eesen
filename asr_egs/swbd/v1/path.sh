77ac9e3931e9b82dc9f86dfe6754c577600cc078
Florian Metze <fmetze@cs.cmu.edu>
2016-05-10 14:10:37 -0700

    fixes on swbd data prep

--- asr_egs/swbd/v1/path.sh
+++ asr_egs/swbd/v1/path.sh
@@ -1,6 +1,7 @@
 export EESEN_ROOT=`pwd`/../../..
-export PATH=$PWD/utils/:$EESEN_ROOT/src/netbin:$EESEN_ROOT/src/featbin:$EESEN_ROOT/src/decoderbin:$EESEN_ROOT/src/fstbin:$EESEN_ROOT/tools/openfst/bin:$EESEN_ROOT/tools/irstlm/bin/:$PWD:$PATH
+export PATH=$PWD/utils/:$EESEN_ROOT/src/netbin:$EESEN_ROOT/src/featbin:$EESEN_ROOT/src/decoderbin:$EESEN_ROOT/src/fstbin:$EESEN_ROOT/tools/openfst/bin:$PWD:$PATH
 export LC_ALL=C
+export LD_LIBRARY_PATH=$EESEN_ROOT/tools/openfst/lib:$LD_LIBRARY_PATH
 
 . $EESEN_ROOT/tools/env.sh
 
@@ -9,4 +10,14 @@ if [[ `uname -n` =~ compute-* ]]; then
     module load python27
     module load gcc-4.9.2
     export TMPDIR=/scratch
+
+elif [[ `uname -n` =~ comet-* ]]; then
+    # SDSC Comet cluster
+    export TMPDIR=/scratch/$USER/$SLURM_JOBID
+fi
+
+if [[ ! -z ${acwt+x} ]]; then
+    # let's assume we're decoding
+    export PATH=$EESEN_ROOT/src-nogpu/netbin:$PATH
+    echo "Preferring non-gpu netbin code"
 fi

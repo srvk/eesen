7526556b0dcd5259a6c51f47933786916ea99670
Florian Metze <fmetze@cs.cmu.edu>
2016-05-10 17:30:03 -0400

    (c)omit

--- asr_egs/swbd/v1/path.sh
+++ asr_egs/swbd/v1/path.sh
@@ -1,4 +1,6 @@
 export EESEN_ROOT=`pwd`/../../..
-export PATH=$PWD/utils/:$EESEN_ROOT/src/netbin:$EESEN_ROOT/src/featbin:$EESEN_ROOT/src/decoderbin:$EESEN_ROOT/src/fstbin:$EESEN_ROOT/tools/openfst/bin:$EESEN_ROOT/tools/irstlm/bin/:$PWD:$PATH
+export PATH=$PWD/utils/:$EESEN_ROOT/src/netbin:$EESEN_ROOT/src/featbin:$EESEN_ROOT/src/decoderbin:$EESEN_ROOT/src/fstbin:$EESEN_ROOT/tools/openfst/bin:$EESEN_ROOT/../kaldi/src/featbin:$PWD:$PATH
 export LC_ALL=C
 
+export TMPDIR=$LOCAL
+. ../../../tools/env.sh

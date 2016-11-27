e404110fd892d76436c9afd99096f0226e602f6d
Florian Metze <fmetze@cs.cmu.edu>
2016-11-27 00:50:50 -0500

    path update

--- asr_egs/hkust/v1/README.md
+++ asr_egs/hkust/v1/README.md
@@ -1,5 +1,5 @@
 
-This recipe builds Chinese Madarin systems with the HKUST Mandarin Telephone Speech corpus.
+This recipe builds Chinese Mandarin systems with the HKUST Mandarin Telephone Speech corpus.
 
 You need to obtain the following 2 LDC datasets:
 
@@ -7,5 +7,4 @@ LDC2005S15 : http://www.ldc.upenn.edu/Catalog/catalogEntry.jsp?catalogId=LDC2005
 
 LDC2005T32 : http://www.ldc.upenn.edu/Catalog/catalogEntry.jsp?catalogId=LDC2005T32
 
-The recipe run_ctc_char.sh models **3600+ Madarin characters** directly. Dictionaries are not required.  
-
+The recipe run_ctc_char.sh models **3600+ Mandarin characters** directly. Dictionaries are not required.
--- asr_egs/hkust/v1/path.sh
+++ asr_egs/hkust/v1/path.sh
@@ -2,3 +2,36 @@ export EESEN_ROOT=`pwd`/../../..
 export PATH=$PWD/utils/:$EESEN_ROOT/src/netbin:$EESEN_ROOT/src/featbin:$EESEN_ROOT/src/decoderbin:$EESEN_ROOT/src/fstbin:$EESEN_ROOT/tools/openfst/bin:$EESEN_ROOT/tools/irstlm/bin/:$PWD:$PATH
 export LC_ALL=C
 
+if [[ `uname -n` =~ comet-* ]]; then
+  :
+elif [[ `uname -n`=~ bridges ]]; then
+  :
+elif [[ `uname -n`=~ compute-* ]]; then
+  # CMU Rocks cluster
+  
+  module load gcc-4.9.2
+  module load cuda-8.0
+  module load python27
+
+  [ -n "$PBS_JOBID" ] && export CUDA_VISIBLE_DEVICES=`qstat -n $PBS_JOBID|awk 'END {split ($NF, a, "/"); printf ("%s\n", a[2])}'`
+
+  export TMPDIR=/scratch
+  export LD_LIBRARY_PATH=/data/ASR1/tools/sox-14.4.2/install/lib:$LD_LIBRARY_PATH
+
+else
+  echo "Which cluster is this?"
+  exit 1;
+fi
+
+[ -f ${EESEN_ROOT}/tools/env.sh ] && . ${EESEN_ROOT}/tools/env.sh
+[ -f ./local.sh ] && . ./local.sh
+
+# for decoding without requiring CUDA
+if [[ "${SLURM_JOB_NAME}" =~ test-* || "${PBS_JOBNAME}" =~ test-* ]]; then
+  echo "Setting path to prefer non-gpu code ..."
+  export PATH=${EESEN_ROOT}/src-nogpu/netbin:${EESEN_ROOT}/src-nogpu/decoderbin:${PATH}
+  export LD_LIBRARY_PATH=${EESEN_ROOT}/tools/openfst/lib:${LD_LIBRARY_PATH}
+  export PATH=${PATH}:/data/ASR5/fmetze/kaldi-latest/src/latbin
+else
+  :
+fi

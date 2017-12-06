62face5c4f08578917d4a0e9af599d7184d62c02
Julian Salazar <julsal@amazon.com>
2017-12-06 00:59:18 -0800

    Resolve 'constexpr' errors from NVCC for GCC 6+

--- src/makefiles/linux_cuda.mk
+++ src/makefiles/linux_cuda.mk
@@ -1,6 +1,6 @@
 
 CUDA_INCLUDE= -I$(CUDATKDIR)/include
-CUDA_FLAGS = -g -Xcompiler -fPIC --verbose --machine 32 -DHAVE_CUDA
+CUDA_FLAGS = -g -std=c++11 -Xcompiler -fPIC --verbose --machine 32 -DHAVE_CUDA
 
 CXXFLAGS += -DHAVE_CUDA -I$(CUDATKDIR)/include 
 LDFLAGS += -L$(CUDATKDIR)/lib -Wl,-rpath=$(CUDATKDIR)/lib
